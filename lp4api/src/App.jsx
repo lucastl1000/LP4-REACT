62cc65a3e74c23a44a194b37e979f6cda6127ee0
lucastl1000 <lucassendsbullet@gmail.com>
2025-08-24 17:48:39 -0400

    criação do projeto utilizando react

---
+++ lp4api/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>IFMS</h1>
+      <h2>LP4 + REACT</h2>
+    </>
+  )
+}
+
+export default App
